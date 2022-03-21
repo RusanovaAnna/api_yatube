3f241436e3122b8f19a739d2433483bdf46d0a89
Русанова Анна <rusanova.annette@gmail.com>
2022-03-21 20:53:09 +0200

    m

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,26 @@
+from posts.models import User
+from rest_framework import serializers
+from posts.models import Post, Group, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field="username", read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = ("post",)
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'image', 'pub_date', 'group', 'comments')
+        model = Post
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ("title", "slug", "description")
+        model = Group
